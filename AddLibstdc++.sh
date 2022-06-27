5ca4315b9de8a932032c4c0ff8952c1f51c223f0
hsec <hi@hsec.one>
2022-06-27 15:46:01 +0800

    new version of c++

--- AddLibstdc++.sh
+++ AddLibstdc++.sh
@@ -9,6 +9,6 @@ sudo cp -v $DIR/dylib/* Contents/Developer/Platforms/iPhoneOS.platform/Library/D
 
 sudo echo Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk/usr/lib/ Contents/Developer/Platforms/DriverKit.platform/Developer/SDKs/DriverKit.sdk/System/DriverKit/usr/lib/ | xargs -n 1 cp -v $DIR/tbd/*
 
-sudo cp -v -r $DIR/include/* Contents/Developer/Platforms/iPhoneOS.platform/Library/Developer/CoreSimulator/Profiles/Runtimes/iOS.simruntime/Contents/Resources/RuntimeRoot/usr/lib/
+sudo cp -v -r $DIR/c++ Contents/Developer/Platforms/iPhoneOS.platform/Library/Developer/CoreSimulator/Profiles/Runtimes/iOS.simruntime/Contents/Resources/RuntimeRoot/usr/lib/
 
 echo "🍻 添加 c++98 & 4.2.1 完成，需重启 Xcode"
