a7efdbe0efdc6cf622982a8b8dad0fa802f5217d
hsec <hi@hsec.one>
2022-06-27 13:56:45 +0800

    测试添加

--- AddLibstdc++.sh
+++ AddLibstdc++.sh
@@ -1,22 +1,14 @@
 # libstdc-form-Xcode9
-echo " - 1 确认已进入 Xcode.app/ 目录"
+echo "请进入 */Xcode.app/ 路径后运行脚本...
+        bash /Users/*/libstdc-form-Xcode9/AddLibstdc++.sh"
 
-if [ $EUID != 0 ]; then
-    sudo "$0" "$@"
-    exit $?
-fi
-echo " - 2"
+DIR=$(dirname "${BASH_SOURCE[0]}")
 
-sudo cp ${BASH_SOURCE[0]}/libstdc++.6.0.9.dylib
-    Contents/Developer/Platforms/iPhoneOS.platform/Library/Developer/CoreSimulator/Profiles/Runtimes/iOS.simruntime/Contents/Resources/RuntimeRoot/usr/lib/
-echo " - 3"
+sudo cp -v $DIR/dylib/* Contents/Developer/Platforms/iPhoneOS.platform/Library/Developer/CoreSimulator/Profiles/Runtimes/iOS.simruntime/Contents/Resources/RuntimeRoot/usr/lib/
 
-sudo echo Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk/usr/lib/ Contents/Developer/Platforms/DriverKit.platform/Developer/SDKs/DriverKit.sdk/System/DriverKit/usr/lib/I xargs -n 1 cp -v ${BASH_SOURCE[0]}/libstdc++.6.0.9.tbd
-    
-#sudo cp ${BASH_SOURCE[0]}/libstdc++.6.0.9.tbd
-#    Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk/usr/lib/
-#
-#sudo cp ${BASH_SOURCE[0]}/libstdc++.6.0.9.tbd
-#    Contents/Developer/Platforms/DriverKit.platform/Developer/SDKs/DriverKit.sdk/System/DriverKit/usr/lib/
-    
-echo "libstdc++ 添加成功"
+
+sudo echo Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk/usr/lib/ Contents/Developer/Platforms/DriverKit.platform/Developer/SDKs/DriverKit.sdk/System/DriverKit/usr/lib/ | xargs -n 1 cp -v $DIR/tbd/*
+
+sudo cp -v -r $DIR/include/* Contents/Developer/Platforms/iPhoneOS.platform/Library/Developer/CoreSimulator/Profiles/Runtimes/iOS.simruntime/Contents/Resources/RuntimeRoot/usr/lib/
+
+echo "🍻 添加 c++98 & 4.2.1 完成，需重启 Xcode"
